3f4e75101b02c67f2b7fae7dca214a523cb6e844
mickeyandkaka <mickeyandkaka@gmail.com>
2017-02-05 23:18:50 +0800

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <assert.h>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+using namespace std;
+
+typedef long long LL;
+typedef pair<int,int> PII;
+typedef vector<int> VI;
+
+#define pb push_back
+#define mp make_pair
+#define all(x)  (x).begin,(x).end()
+#define rep(i, a)   for(int i=0; i<(int)a.size(); i++)
+#define clr(a, b)   memset(a, b, sizeof(a))
+#define show(x) cerr << x
+#define debug(x) show(#x << ": " << (x) << endl)
+
+
+int main()
+{
+    #ifdef LOCAL
+        freopen("in", "r", stdin);
+        //freopen("out", "w", stdout);
+    #endif
+
+
+
+    return 0;
+}
